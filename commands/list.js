be164469ac7f7d76a55fe3b5371dfebc2aba2af3
Sofia <sofia@MacBook-Air-Sofia.local>
2024-11-17 01:12:22 +0300

    add list command

--- commands/list.js
+++ commands/list.js
@@ -1,26 +1,27 @@
-const { SlashCommandBuilder, EmbedBuilder, InteractionContextType } = require('discord.js');
+const { SlashCommandBuilder, EmbedBuilder } = require('discord.js');
 
 const command = new SlashCommandBuilder()
-	.setName('list')
-	.setDescription('your anime list')
-	.setContexts(InteractionContextType.BotDM, InteractionContextType.Guild)
+    .setName('list')
+    .setDescription('your anime list')
 	.addIntegerOption(option =>
-		option.setName('page')
-			.setDescription('Page of list')
-			.setMinValue(1)
+		option.setName("page")
+		.setDescription("Page number to view your anime")
 	);
 
-const embed = new EmbedBuilder()
-	.setColor(0x0099FF)
-	.setTitle('Your watched anime')
-	.setAuthor({ name: 'Monkey team', url: 'https://github.com/JustCursed/monkey-project' })
-	.setTimestamp();
 
 module.exports = {
-	data: command,
-	execute: async ctx => {
-		// await ctx.reply({ embeds: [embed] });
-		console.log(await ctx.client.db.getAniList(ctx.user.id, ctx.options.getInteger('page') ?? 1));
-		await ctx.reply('dwad');
-	}
+    data: command,
+    execute: async ctx => {
+		const embed = new EmbedBuilder()
+			.setColor(0x0099FF)
+			.setTitle('Your wached anime')
+			.setAuthor({ name: 'Monkey team', url: 'https://github.com/JustCursed/monkey-project' })
+			.setThumbnail(ctx.user.avatarURL())
+			.setTimestamp()
+		const userId = ctx.user.id
+		const page = ctx.options.getInteger('page') ?? 1;
+		const aniList = await ctx.client.db.getAniList(userId, page)
+		aniList.forEach(anime => embed.addFields({ name: anime.name, value: new Date(anime.time).toISOString() }) );
+        ctx.reply({ embeds: [embed], ephemeral: true });
+    }
 };
